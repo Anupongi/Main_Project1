6241f620e2bad01dd5571ebd32aca3e8dc7b85bc
Icesolo <jmfairy123@hotmail.com>
2019-09-22 19:25:50 +0700

    lastupdate

--- page/upload_download/upload.php
+++ page/upload_download/upload.php
@@ -348,9 +348,9 @@ $num_rows2 = mysqli_num_rows($query2);
                       if (move_uploaded_file($file, $destination)) {
                           $conn2 = mysqli_connect('localhost','root','KZTuR1v3aaVA7t','file-management');
                           $sql = "INSERT INTO `files` (`image`, `name`, `extension` , `size`, `downloads`, `user_post`, `date`, `published`) VALUES ('$file_image','$filename','$extension', $size, 0 ,'$id_admin' ,'$date1', 'y')";
-                          echo $sql;
+                          
                           $result = mysqli_query($conn2, $sql);
-                          if (mysqli_query($conn2, $sql)) {
+                          if ($result == 1) {
                               
                               $alert = '<div class="alert alert-success" role="alert">เอกสารของคุณถูกเพิ่มเรียบร้อยแล้ว <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
