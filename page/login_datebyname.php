f0df129e1ff58cb5f23fe106eb26b968a115bc0c
Icesolo <jmfairy123@hotmail.com>
2019-09-24 00:35:54 +0700

    lastupdate

--- page/login_datebyname.php
+++ page/login_datebyname.php
@@ -422,25 +422,10 @@ $per_page = 10;   // Per Page
                                         <?php
                                         }
                                         }else{
-                                            echo '
-                                            <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
-                                            
-                                              <!-- Then put toasts within -->
-                                              <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                                                <div class="toast-header">
-                                                  <img src="..." class="rounded mr-2" alt="...">
-                                                  <strong class="mr-auto">Bootstrap</strong>
-                                                  <small>11 mins ago</small>
-                                                  <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                  </button>
-                                                </div>
-                                                <div class="toast-body">
-                                                  Hello, world! This is a toast message.
-                                                </div>
-                                              </div>
-                                            </div>
-                                            ';
+                                            echo "<script type='text/javascript'>";
+				                            echo "alert('ผิดพลาด ไม่พบชื่อผู้ใช้นี้');";
+				                            echo "window.location='./login_datebyname.php';";
+			                                echo "</script>";
                                         }
                                         }
                                         else{
