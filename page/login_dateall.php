54ea28a7b0f1794a0f79607c1c7e99afb00a9269
Icesolo <jmfairy123@hotmail.com>
2019-09-23 09:56:26 +0700

    lastupdate

--- page/login_dateall.php
+++ page/login_dateall.php
@@ -401,7 +401,7 @@ $per_page = 10;   // Per Page
                                             </tr>
                                         </thead>
                                         <?php
-                                        $conn = mysqli_connect("localhost","root","","user_login");
+                                        $conn = mysqli_connect("localhost","root","KZTuR1v3aaVA7t","user_login");
                                         $count = 0;
                                         mysqli_set_charset($conn, "utf8");
                                         if(isset($_POST['datetimepicker1'])){
