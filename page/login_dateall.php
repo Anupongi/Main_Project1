126827f3ce75a7ae02ccbd4152bb4dc0cf7ee4bd
Icesolo <jmfairy123@hotmail.com>
2019-09-23 13:19:44 +0700

    lastupdate

--- page/login_dateall.php
+++ page/login_dateall.php
@@ -479,7 +479,7 @@ $per_page = 10;   // Per Page
                                         }}
                                         if(isset($_POST['id_num'])){
                                           $id_user = $_POST['id_num'];
-                                          $sql ="SELECT login_date.Username, user.Firstname, user.Lastname , login_date.Lastdate FROM login_date INNER JOIN user ON login_date.Username=user.Username WHERE login_date.Username = '$id_user'  ORDER BY `Lastdate` DESC";
+                                          $sql ="SELECT login_date.Username, user.Firstname, user.Lastname , login_date.Lastdate FROM login_date INNER JOIN user ON login_date.Username=user.Username WHERE login_date.Username LIKE '%$id_user%' ORDER BY `Lastdate` DESC";
                                           echo $sql;
                                           $query = mysqli_query($conn,$sql);
                                           while ($d = mysqli_fetch_array($query)) {
