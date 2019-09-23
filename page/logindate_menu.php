f82e85e8cf89f953ca6716d3513cbb1e3df1a6ba
Icesolo <jmfairy123@hotmail.com>
2019-09-23 14:56:43 +0700

    lastupdate

--- page/logindate_menu.php
+++ page/logindate_menu.php
@@ -362,13 +362,13 @@ $per_page = 10;   // Per Page
                     <div class="form-row text-center " style="BORDER-BOTTOM: #b7deff 1px dashed;BORDER-TOP: #b7deff 1px dashed;BORDER-LEFT: #b7deff 1px dashed;BORDER-RIGHT: #b7deff 1px dashed;">
                         
                         <div class="col pt-4 pb-4">
-                            <button type="button" class="btn btn-primary">ค้นหาจากช่วงวันที่</button>
+                            <button type="button" class="btn" style="color:Orange;">ค้นหาจากช่วงวันที่</button>
                         </div>
                         <div class="col pt-4 pb-4">
-                            <button type="button" class="btn btn-secondary">ค้นหาจากวันที่</button>
+                            <button type="button" class="btn" style="color:hsl(147, 50%, 47%);">ค้นหาจากวันที่</button>
                         </div>
                         <div class="col pt-4 pb-4">
-                            <button type="button" class="btn btn-success">ค้นหาจากชื่อผู้ใช้</button>
+                            <button type="button" class="btn" style="color:Tomato;">ค้นหาจากชื่อผู้ใช้</button>
                         </div>
                         
                     </div>
