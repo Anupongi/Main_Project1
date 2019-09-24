a70e52f6db2409b5bb41d9323486e73dcfad1810
Icesolo <jmfairy123@hotmail.com>
2019-09-24 10:56:17 +0700

    lastupdate

--- page/login_datebydate.php
+++ page/login_datebydate.php
@@ -361,11 +361,11 @@ $per_page = 10;   // Per Page
               <div class="col-md-6">
                 <form action="" method="post">
                     <div class="form-row">
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-5">
                             <label for="inputEmail4">ค้นหาวันที่</label>
-                            <input class="form-control col-md-5" name="datetimepicker1" id="datetimepicker2" date-picker-example type="text" data-provide="datepicker" data-date-language="th-th" data-date-format="dd/mm/yyyy" value="<?php echo $date ?>" placeholder="xx/xx/xxxx" autocomplete="off">
+                            <input class="form-control col-md-7" name="datetimepicker1" id="datetimepicker2" date-picker-example type="text" data-provide="datepicker" data-date-language="th-th" data-date-format="dd/mm/yyyy" value="<?php echo $date ?>" placeholder="xx/xx/xxxx" autocomplete="off">
                         </div>
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-4">
                             <label for="inputPassword4"></label>
                             <button type="submit" class="btn btn-info">ค้นหา</button>
                         </div>
