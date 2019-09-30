ca8dba33dbe3a74e281e22fbc8d874240095f7cc
Icesolo <jmfairy123@hotmail.com>
2019-09-30 19:02:05 +0700

    lastupdate

--- Login/index.php
+++ Login/index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="../img/icons.ico">
+<link rel="icon" type="image/png" href="../img/206-2067143_no-wait-emergency-room-medical-bed-icon.png" >
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
