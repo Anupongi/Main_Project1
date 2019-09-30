665c9893c8c2418ae31439372cb4295fa2ae47fb
Icesolo <jmfairy123@hotmail.com>
2019-09-30 17:42:10 +0700

    lastupdate

--- admin_index.php
+++ admin_index.php
@@ -137,7 +137,7 @@ $num_rows5 = mysqli_num_rows($query5);
     <a href="index1.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">สำหรับเจ้าหน้าที่</span>
     </a>
 
     <!-- Sidebar -->
