026e9cf26f334b269330e80aa4b0d462264d6ae4
Icesolo <jmfairy123@hotmail.com>
2019-10-02 10:19:49 +0700

    lastupdate

--- user/user_index.php
+++ user/user_index.php
@@ -2,7 +2,7 @@
   session_start();
   
   if(!isset($_SESSION["UserID"])){
-    header('Location: ./Login/index.php');
+    header('Location: ../Login/index.php');
   }
 ?>  
 <!DOCTYPE html>
@@ -17,27 +17,27 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../plugins/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="plugins/morris/morris.css">
+  <link rel="stylesheet" href="../plugins/morris/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="../plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="../plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
+  <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker-bs3.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <link rel="icon" href="./img/5d91b4ef7a7d7.256px.ico" type="image/png">
-  <link rel="icon" type="image/png" href="./img/206-2067143_no-wait-emergency-room-medical-bed-icon.png" >
+  
+  <link rel="icon" type="image/png" href="../dist/img/206-2067143_no-wait-emergency-room-medical-bed-icon.png" >
 </head>
 <body class="hold-transition sidebar-mini">
 <?php
@@ -89,14 +89,11 @@ if(mysqli_num_rows($result2)==1){
       <a href="#" class="nav-link"><i class="ion-ios-home"></i> หน้าแรก</a>
       </li>
     </ul>
-
-
-    
     <ul class="navbar-nav ml-auto">
       <!-- Messages Dropdown Menu -->
       <li>
       <?php
-        echo '<a href="./logout.php"><i class="fa fa-indent"></i> ออกจากระบบ</a> ';
+        echo '<a href="../Login/logout.php"><i class="fa fa-indent"></i> ออกจากระบบ</a> ';
       ?>
         
           
@@ -112,7 +109,7 @@ if(mysqli_num_rows($result2)==1){
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <p class="brand-link">
-        <img src="./dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
            <?php 
            $_SESSION["Userlevel"];
            if($_SESSION["Userlevel"] == 02){
@@ -135,7 +132,7 @@ if(mysqli_num_rows($result2)==1){
           $query = mysqli_query($conn,$sql1);
           while ($d = mysqli_fetch_array($query)) {
           ?>
-          <img src="./user/img/img/profile/<?php echo $d[5]?>" class="img-circle elevation-2" alt="User Image" style="width:40px;height:40px;">
+          <img src="./img/img/profile/<?php echo $d[5]?>" class="img-circle elevation-2" alt="User Image" style="width:40px;height:40px;">
           <?php
           }
           ?>
@@ -193,13 +190,13 @@ if(mysqli_num_rows($result2)==1){
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./user/alllist_treatment.php" class="nav-link">
+                <a href="./alllist_treatment.php" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>ประวัติการรักษาทั้งหมด</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./user/list_treatment.php" class="nav-link">
+                <a href="./list_treatment.php" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>ประวัติการรักษาประจำวัน</p>
                 </a>
@@ -216,13 +213,13 @@ if(mysqli_num_rows($result2)==1){
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./user/alllist_login.php" class="nav-link">
+                <a href="./alllist_login.php" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>การลงชื่อเข้าใช้ทั้งหมด</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./user/list_login.php" class="nav-link">
+                <a href="./list_login.php" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>การลงชื่อเข้าใช้ประจำวัน</p>
                 </a>
@@ -269,7 +266,7 @@ if(mysqli_num_rows($result2)==1){
               <div class=" text-center d-flex align-items-center rgba-black-strong py-2 px-4">
                 <div class="card-body">
                   <h5 class="card-text" style="color: white; text-shadow: 2px 2px 4px #000000;">แสดงการลงชื่อเข้าใช้บริการห้องพยาบาล</h5>
-                  <a href="./user/alllist_login.php" class="btn btn-primary">ข้อมูลเพิ่มเติม</a>
+                  <a href="./alllist_login.php" class="btn btn-primary">ข้อมูลเพิ่มเติม</a>
                 </div>
               </div>
               <div class="card-footer text-muted text-center">
@@ -285,7 +282,7 @@ if(mysqli_num_rows($result2)==1){
                 <div class=" text-center d-flex align-items-center rgba-black-strong py-2 px-4">
                   <div class="card-body">
                     <h5 class="card-text" style="color: white; text-shadow: 2px 2px 4px #000000;">แสดงการประวัติการรักษาของผู้ใช้</h5>
-                    <a href="./user/alllist_treatment.php" class="btn btn-primary">ข้อมูลเพิ่มเติม</a>
+                    <a href="./alllist_treatment.php" class="btn btn-primary">ข้อมูลเพิ่มเติม</a>
                   </div>
                 </div>
                 <div class="card-footer text-muted text-center">
@@ -317,7 +314,7 @@ if(mysqli_num_rows($result2)==1){
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="../plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -325,34 +322,34 @@ if(mysqli_num_rows($result2)==1){
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Morris.js charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="plugins/morris/morris.min.js"></script>
+<script src="../plugins/morris/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="../plugins/sparkline/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="../plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="../plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="plugins/knob/jquery.knob.js"></script>
+<script src="../plugins/knob/jquery.knob.js"></script>
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src="../plugins/daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="../plugins/datepicker/bootstrap-datepicker.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
+<script src="../plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src="../dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="../dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="../dist/js/demo.js"></script>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
