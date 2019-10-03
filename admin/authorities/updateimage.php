901440917957e62540cb9ff4aee89b77000ec5f0
Icesolo <jmfairy123@hotmail.com>
2019-10-03 15:15:04 +0700

    lastupdate

--- admin/authorities/updateimage.php
+++ admin/authorities/updateimage.php
@@ -7,6 +7,7 @@ $dbName = "user_login";
 $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
 $id = $_POST['id'];
+
 $file_name = $_FILES['image']['name'];
 $file_tmp =$_FILES['image']['tmp_name'];
 move_uploaded_file($file_tmp,"./profile/".$file_name);
