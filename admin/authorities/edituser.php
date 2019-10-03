e0ffc0d3e7016e5b33e24fd3a03df487c6156eab
Icesolo <jmfairy123@hotmail.com>
2019-10-03 14:49:49 +0700

    lastupdate

--- admin/authorities/edituser.php
+++ admin/authorities/edituser.php
@@ -56,7 +56,7 @@ $query = mysqli_query($conn,$sql);
 $num_rows = mysqli_num_rows($query);
 
 $id = $_GET['ID'];
-
+echo $id;
 mysqli_set_charset($conn,"utf8");
 $sql="SELECT * FROM `user` WHERE `ID` = $id ";
 // echo $sql;
@@ -354,7 +354,7 @@ $array = mysqli_fetch_array($query);
                                         <div class="profile-image"> 
                                           <div class="col-lg-12  text-center">
                                             <?php
-                                              $iduser = ''
+                                              $iduser = '';
                                               mysqli_set_charset($conn,"utf8");
                                               $sqlimg="SELECT * FROM `user` WHERE `Username` = '$id' ";
                                               echo $sqlimg;
