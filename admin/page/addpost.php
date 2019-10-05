83956045d503c0007b9f6944dd78eeef2132b4b5
Icesolo <jmfairy123@hotmail.com>
2019-10-06 00:30:29 +0700

    lastupdate

--- admin/page/addpost.php
+++ admin/page/addpost.php
@@ -421,10 +421,7 @@ $num_rows1 = mysqli_num_rows($query1)
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
 <script>
-  CKEDITOR.replace('editor1'{
-    filebrowserBrowseUrl: '/browser/browse.php',
-    filebrowserUploadUrl: '/uploader/upload.php'
-  });
+  CKEDITOR.replace('editor1');
 </script>
 <script>
            $('input[type="file"]').change(function(e){
