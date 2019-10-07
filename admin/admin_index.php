867c19ec66d43278d92f876c121af7e176c5de62
Icesolo <jmfairy123@hotmail.com>
2019-10-07 23:39:48 +0700

    lastupdate

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -2,17 +2,17 @@
   session_start();
   
   if(!isset($_SESSION["UserID"])){
-    header('Location: ./Login/index.php');
+    header('Location: ../Login/index.php');
   }
   //กำหนดเวลาที่สามารถอยู่ในระบบ
-  $sessionlifetime = 60; //กำหนดเป็นนาที
+  $sessionlifetime = 5; //กำหนดเป็นนาที
  
     if(isset($_SESSION["timeLasetdActive"])){
 	    $seclogin = (time()-$_SESSION["timeLasetdActive"])/60;
 	    //หากไม่ได้ Active ในเวลาที่กำหนด
 	  if($seclogin>$sessionlifetime){
 		  //goto logout page
-		  header("location: ./Login/logoutForSes.php");
+		  header("location: ../Login/logoutForSes.php");
 		exit;
 	  }else{
 		  $_SESSION["timeLasetdActive"] = time();
