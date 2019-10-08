ba7b0fb0b15136f50799c0ad93a12ef3d66dba72
Icesolo <jmfairy123@hotmail.com>
2019-10-08 10:43:56 +0700

    lastupdate

--- Login/logoutForSes.php
+++ Login/logoutForSes.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: ./Login/index.php?state=sessiontimeout");	
+header("Location: index.php?state=sessiontimeout");	
 ?>
 
--- admin/admin_index.php
+++ admin/admin_index.php
@@ -12,7 +12,7 @@
 	    //หากไม่ได้ Active ในเวลาที่กำหนด
 	  if($seclogin>$sessionlifetime){
 		  //goto logout page
-		  header("location: ../logoutForSes.php");
+		  header("location: ../Login/logoutForSes.php");
 		exit;
 	  }else{
 		  $_SESSION["timeLasetdActive"] = time();
