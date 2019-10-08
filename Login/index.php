069350396bb8513b68a4b9076658bd93cab6af28
Icesolo <jmfairy123@hotmail.com>
2019-10-08 22:36:56 +0700

    lastupdate

--- Login/index.php
+++ Login/index.php
@@ -168,7 +168,7 @@ session_start();
 					data: json,
 					success: function(response) {
             	if (response == 1) {
-							window.location.href = "./check.php";
+					window.location.href = "../admin/admin_index.php";
 				}else{
 							Swal.fire({
 								type: 'error',
