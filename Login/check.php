7a0c867ca86f79b2243c7faa83fc8fa76e529fe4
Icesolo <jmfairy123@hotmail.com>
2019-10-09 14:23:34 +0700

    lastupdate

--- Login/check.php
+++ Login/check.php
@@ -24,11 +24,10 @@ session_start();
                       $_SESSION["Userlevel"] = $row["Userlevel"];
                       
                       
-                      // if($_SESSION["Userlevel"]=="01"){ //ถ้าเป็น admin ให้กระโดดไปหน้า admin_page.php
-                      //   echo 1;
-                      //   Header("Location: ../admin/admin_index.php");
-
-                      // }
+                      if($_SESSION["Userlevel"]=="01"){ //ถ้าเป็น admin ให้กระโดดไปหน้า admin_page.php
+                        // Header("Location: ../admin/admin_index.php");
+                        echo "window.location.href = '../admin/admin_index.php';"
+                      }
 
                       // if ($_SESSION["Userlevel"]=="02"){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
 
