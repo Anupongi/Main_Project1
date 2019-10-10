165f8adecb6f21cc597c24aa7b0b805dbc1733e3
Icesolo <jmfairy123@hotmail.com>
2019-10-10 23:52:56 +0700

    lastupdate

--- admin/treatment/treatment.php
+++ admin/treatment/treatment.php
@@ -38,6 +38,8 @@
   <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
   <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
   <link rel="icon" type="image/png" href="../../dist/img/206-2067143_no-wait-emergency-room-medical-bed-icon.png" >
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@8.18.1/dist/sweetalert2.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.18.1/dist/sweetalert2.min.js"></script> 
   <style>
     .bg {
   height: 100%;
