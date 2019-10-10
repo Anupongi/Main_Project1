2f606d9b599abfc48716ccd0318ff723f15e4d65
Icesolo <jmfairy123@hotmail.com>
2019-10-10 14:09:26 +0700

    lastupdate

--- admin/treatment/deluser.php
+++ admin/treatment/deluser.php
@@ -13,17 +13,3 @@
 
 ?>
 
-<!--
-    $serverName = "localhost";
-    $userName = "root";
-    $userPassword = "Ice@2019";
-    $dbName = "treatment";
-    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
-    $id =  $_POST['ID'];
-    $sqlDele = "DELETE FROM `tb_med` WHERE `med_id` = $id";
-    $queryDele = mysqli_query($con,$sqlDele);
-    // echo 1;
-    echo $sqlDele;
-    // header('Location: treatment_list.php');
-
--->
