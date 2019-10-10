215fb61e789515866a34a44b2c70e01ae22ef03a
Icesolo <jmfairy123@hotmail.com>
2019-10-10 13:37:21 +0700

    lastupdate

--- admin/treatment/treatment_list1.php
+++ admin/treatment/treatment_list1.php
@@ -480,11 +480,11 @@ $per_page = 10;   // Per Page
                                             <script>
                                               $(document).ready(function() {
                                               $(".submit_<?php echo $result[0];?>").click(function() {
-                                                var delete = <?php echo $result[0];?>;
-                                                var json = {
-                                                  ID: delete
-                                                };
-                                                    console.log(json);
+                                                // var delete = <?php echo $result[0];?>;
+                                                // var json = {
+                                                //   ID: delete
+                                                // };
+                                                    console.log("json");
                                                 // $.ajax({
                                                 //   type: "post",
                                                 //   url: "./check.php",
