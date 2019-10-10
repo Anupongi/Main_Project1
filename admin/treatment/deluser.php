f2dbdc381a0b71f2c758e1a8e6dc4c86302d7802
Icesolo <jmfairy123@hotmail.com>
2019-10-10 13:52:17 +0700

    lastupdate

--- admin/treatment/deluser.php
+++ admin/treatment/deluser.php
@@ -4,9 +4,10 @@
     $userPassword = "Ice@2019";
     $dbName = "treatment";
     $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
-    $id =  $_GET['ID'];
+    $id =  $_POST['ID'];
     $sqlDele = "DELETE FROM `tb_med` WHERE `med_id` = $id";
     $queryDele = mysqli_query($con,$sqlDele);
-    header('Location: treatment_list.php');
-    
+    echo 1;
+    // header('Location: treatment_list.php');
+
 ?>
--- admin/treatment/treatment_list1.php
+++ admin/treatment/treatment_list1.php
@@ -486,17 +486,31 @@ $per_page = 10;   // Per Page
                                                   ID: delete1
                                                 };
                                                     console.log("json");
+                                                    Swal.fire({
+                                                      title: 'Are you sure?',
+                                                      text: "You won't be able to revert this!",
+                                                      type: 'warning',
+                                                      showCancelButton: true,
+                                                      confirmButtonColor: '#3085d6',
+                                                      cancelButtonColor: '#d33',
+                                                      confirmButtonText: 'Yes, delete it!'
+                                                    }).then((result) => {
+                                                      if (result.value) {
+                                                        Swal.fire(
+                                                          'Deleted!',
+                                                          'Your file has been deleted.',
+                                                          'success'
+                                                        )
+                                                      }
+                                                    })
+
+
                                                 // $.ajax({
                                                 //   type: "post",
-                                                //   url: "./check.php",
+                                                //   url: "./deluser.php",
                                                 //   data: json,
                                                 //   success: function(response) {
-                                                //       if (response == 3) {
-                                                //   window.location.href = "../admin/admin_index.php";
-                                                // }else if(response == 4){
-                                                //   window.location.href = "../user/user_index.php";
-                                                // }else if(response == 5){
-                                                //   window.location.href = "../user/user_index.php";
+                                                      
                                                 // }else{
                                                 //       Swal.fire({
                                                 //         type: 'error',
