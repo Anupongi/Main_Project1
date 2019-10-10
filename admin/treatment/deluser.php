425f0a68c16e73b50a6b27a266c603188c560a78
Icesolo <jmfairy123@hotmail.com>
2019-10-10 14:11:51 +0700

    lastupdate

--- admin/treatment/deluser.php
+++ admin/treatment/deluser.php
@@ -5,10 +5,10 @@
     $dbName = "treatment";
     $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
     $id =  $_POST['ID'];
-    $sqlDele = "DELETE FROM `tb_treatment` WHERE `med_id` = $id";
+    $sqlDele = "DELETE FROM `tb_treatment` WHERE `id` = $id";
     $queryDele = mysqli_query($con,$sqlDele);
-    // echo 1;
-    echo $sqlDele;
+    echo 1;
+    // echo $sqlDele;
     // header('Location: treatment_list.php');
 
 ?>
