ca87c596d9aa276d2db1773205f6ce7f8e00c260
Icesolo <jmfairy123@hotmail.com>
2019-10-14 15:06:42 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -4,6 +4,6 @@
     $sql ="SELECT * FROM `user` WHERE Username = '$id'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
-    $_SESSION["Userlevel"] = $row["Userlevel"];
-    echo $_SESSION["Userlevel"];
+    $userlevel = $row["Userlevel"];
+    echo $userlevel;
 ?>
