3a8390237693f171ee799a97cc39d2c3fc0202b6
Icesolo <jmfairy123@hotmail.com>
2019-10-14 16:30:31 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -7,7 +7,7 @@
     // $array = mysqli_fetch_array($result);
         if(mysqli_num_rows($result)==1){
             $row = mysqli_fetch_array($result);
-            $ID = $row["id"];
+            $ID = $row["ID"];
             echo $ID;
             $userlevel = $row["Userlevel"];
             // if($userlevel == '02'){
