e26bbb687e25019b0d1c130991240097111a06c0
Icesolo <jmfairy123@hotmail.com>
2019-10-14 15:23:50 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -4,7 +4,7 @@
     $sql ="SELECT * FROM `user` WHERE Username = '$id'";
     echo $sql;
     $result = mysqli_query($con, $sql);
-    $array = mysqli_fetch_array($result);
+    // $array = mysqli_fetch_array($result);
         if(mysqli_num_rows($result)==1){
             $row = mysqli_fetch_array($result);
             $userlevel = $row["Userlevel"];
