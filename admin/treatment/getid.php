ebe8ce27941b7bf600e3d02aaf5196eb95be5df7
Icesolo <jmfairy123@hotmail.com>
2019-10-14 15:00:14 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -1,7 +1,7 @@
 <?php
     include "../../connection/connection.php";
     $id = $_GET['ID'];
-    $sql ="SELECT * FROM `user` WHERE id = '$id'";
+    $sql ="SELECT * FROM `user` WHERE Username = '$id'";
     echo $sql;
     $result = mysqli_query($conn, $sql);
     
