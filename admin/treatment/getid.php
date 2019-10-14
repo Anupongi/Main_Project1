d5cfda402de8e1c2849c2d0910c942b14e573e4b
Icesolo <jmfairy123@hotmail.com>
2019-10-14 16:41:47 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -6,13 +6,18 @@
     echo $sql;
     $result = mysqli_query($connn, $sql);
     // $array = mysqli_fetch_array($result);
-        // if(mysqli_num_rows($result)==1){
-        //     $row = mysqli_fetch_array($result);
-        //     $ID = $row["ID"];
-        //     echo $ID;
-        //     $userlevel = $row["Userlevel"];
-        //     // if($userlevel == '02'){
-        //     //     Header("Location: ./test.php?ID=$id");
-        //     // }
-        // }
+        if(mysqli_num_rows($result)==1){
+            $row = mysqli_fetch_array($result);
+            $userid = $row["Student_id"];
+            echo $userid;
+            // include "../../connection/connection.php";
+            // $sql = "SELECT * FROM `user` ";
+            // $row = mysqli_fetch_array($result);
+            // $ID = $row["ID"];
+            // echo $ID;
+            // $userlevel = $row["Userlevel"];
+            // if($userlevel == '02'){
+            //     Header("Location: ./test.php?ID=$id");
+            // }
+        }
 ?>
