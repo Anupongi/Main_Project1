b35fc3fd23cfbf4915e65123fef56a8749bf4d4c
Icesolo <jmfairy123@hotmail.com>
2019-10-14 16:37:15 +0700

    lastupdate

--- admin/treatment/getid.php
+++ admin/treatment/getid.php
@@ -1,17 +1,18 @@
 <?php
-    include "../../connection/connection.php";
+    include "../../connection/connection3.php";
+    // include "../../connection/connection.php";
     $id = $_GET['ID'];
-    $sql ="SELECT * FROM `user` WHERE Username = '$id'";
+    $sql ="SELECT * FROM `tb_treatment` WHERE id = '$id'";
     echo $sql;
-    $result = mysqli_query($con, $sql);
+    $result = mysqli_query($connn, $sql);
     // $array = mysqli_fetch_array($result);
-        if(mysqli_num_rows($result)==1){
-            $row = mysqli_fetch_array($result);
-            $ID = $row["ID"];
-            echo $ID;
-            $userlevel = $row["Userlevel"];
-            // if($userlevel == '02'){
-            //     Header("Location: ./test.php?ID=$id");
-            // }
-        }
+        // if(mysqli_num_rows($result)==1){
+        //     $row = mysqli_fetch_array($result);
+        //     $ID = $row["ID"];
+        //     echo $ID;
+        //     $userlevel = $row["Userlevel"];
+        //     // if($userlevel == '02'){
+        //     //     Header("Location: ./test.php?ID=$id");
+        //     // }
+        // }
 ?>
---
+++ connection/connection3.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = "localhost"; //ชื่อโฮสต์
+$user = "root"; //ชื่อผู้ใช้
+$password = "Ice@2019"; //รหัสผ่าน
+$dbname = "treatment"; //ชื่อฐานข้อมูล
+$tblname = "tb_treatment"; //ชื่อตาราง 
+$connn= mysqli_connect($hostname, $user, $password,$dbname) or die("Error: " . mysqli_error($con));
+mysqli_query($conn, "SET NAMES 'utf8' "); 
+?>
