58fd61fead561af9b18188d4377f353609a22009
Icesolo <jmfairy123@hotmail.com>
2019-11-13 23:26:00 +0700

    lastupdate

--- admin/page/logindate_menu.php
+++ admin/page/logindate_menu.php
@@ -437,7 +437,7 @@ $per_page = 10;   // Per Page
                                 <?php
                                 $conn = mysqli_connect("localhost","root","Ice@2019","user_login");
                                 mysqli_set_charset($conn, "utf8");
-                                $sql ="SELECT login_date.Username, user.Firstname, user.Lastname , login_date.Lastdate,login_date.time FROM login_date INNER JOIN user ON login_date.Username=user.Username";
+                                $sql ="SELECT login_date.Username, user.Firstname, user.Lastname , login_date.Lastdate,login_date.time FROM login_date INNER JOIN user ON login_date.Username=user.Username ORDER BY `login_date`.`Lastdate` ASC, `login_date`.`time` DESC";
                                 // echo $sql;
                                 $query = mysqli_query($conn,$sql);
                                 $count = 0;
