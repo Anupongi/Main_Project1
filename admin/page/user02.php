84c6c0550cf2d3d9ae0313664620dc24f6c654c2
Icesolo <jmfairy123@hotmail.com>
2019-11-13 16:01:48 +0700

    lastupdate

--- admin/page/user02.php
+++ admin/page/user02.php
@@ -7,7 +7,7 @@
  <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css"> -->
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
- <link rel="stylesheet" href="../dist/css/alluser.css">
+ <link rel="stylesheet" href="../../dist/css/alluser.css">
  </head>
  <body style="margin-top: 10px;font-family: 'Kanit', sans-serif;">
  <?php
