61851403a3d75fc6fe8e530e0c053cb302b00fe8
Icesolo <jmfairy123@hotmail.com>
2019-11-14 00:31:52 +0700

    lastupdate

--- admin/page/login_datebyduration.php
+++ admin/page/login_datebyduration.php
@@ -55,11 +55,7 @@ mysqli_set_charset($conn, "utf8");
 if(isset($_POST['datetimepicker1'])){  
   $datestart = $_POST['datetimepicker1'];
   $dateend = $_POST['datetimepicker2'];
-
-}else{
-  
-}
-$sql = "SELECT * FROM `login_date`";
+  $sql = "SELECT * FROM `login_date` WHERE `Lastname` = '$datestart' AND $dateend";
 $query = mysqli_query($conn,$sql);
 
 $num_rows = mysqli_num_rows($query);
@@ -97,6 +93,48 @@ $per_page = 10;   // Per Page
     
     $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
 	$query = mysqli_query($conn,$sql);
+
+}else{
+ $sql = "SELECT * FROM `login_date`";
+$query = mysqli_query($conn,$sql);
+
+$num_rows = mysqli_num_rows($query);
+
+$per_page = 10;   // Per Page
+	$page  = 1;
+	
+	if(isset($_GET["Page"]))
+	{
+		$page = $_GET["Page"];
+	}
+
+	$prev_page = $page-1;
+	$next_page = $page+1;
+
+	$row_start = (($per_page*$page)-$per_page);
+	if($num_rows<=$per_page)
+	{
+		$num_pages =1;
+	}
+	else if(($num_rows % $per_page)==0)
+	{
+		$num_pages =($num_rows/$per_page) ;
+	}
+	else
+	{
+		$num_pages =($num_rows/$per_page)+1;
+		$num_pages = (int)$num_pages;
+	}
+	$row_end = $per_page * $page;
+	if($row_end > $num_rows)
+	{
+		$row_end = $num_rows;
+	}
+    
+    $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
+	$query = mysqli_query($conn,$sql); 
+}
+
 
 
 ?>
