6cd2a3a9be2b46592e831311e01c6a470d965400
Icesolo <jmfairy123@hotmail.com>
2019-11-13 22:11:38 +0700

    lastupdate

--- admin/page/user03.php
+++ admin/page/user03.php
@@ -61,74 +61,89 @@
 	$sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
 	$query = mysqli_query($conn,$sql);
 ?>
-<h1>รายชื่อคณะครู บุคลากรผู้สมัครสมาชิก</h1>
-<table align="center">
-<thead>
-    <tr>
-        <th width="91"> <div align="center">ลำดับ</div></th>
-        <th width="110"> <div align="center">ชื่อผู้ใช้ </div></th>
-        <th width="120"> <div align="center">รหัสผ่าน </div></th>
-        <th width="150"> <div align="center">ชื่อ-นามสกุล </div></th>
-        <th width="80"><div align="center">สถานะผู้ใช้</div></th>
-        <th width="100"><div align="center">เริ่มสมัครสมาชิก</div></th>
-        <!-- <th width="59"> <div align="center">แก้ไข </div></th> -->
-        <th width="71"> <div align="center">ลบ </div></th>
-    </tr>
-  </thead>
-<?php
-$count = 1;
-while($result=mysqli_fetch_array($query))
-{
-?>
-  <tr>
-    <td><div align="center"><?php echo $count;?></div></td>
-    <td><?php echo $result["Username"];?></td>
-    <td><?php echo $result["Password"];?></td>
-    <td><div align="center"><?php echo $result["Firstname"] ." ".$result["Lastname"];?></div></td>
-	<td><?php 
-	$level = $result["Userlevel"];
-	$sql1 = "SELECT * FROM `user_level` WHERE `userlevel_id` = $level ";
-	$query1 = mysqli_query($conn,$sql1);
-	$userlevel = mysqli_fetch_array($query1);
-	echo $userlevel[1];
-	
-	
-	?></td>
-    <td align="right"><?php echo $result["date"];?></td>
-    <!-- <td align="right"><a href="./edituser.php?ID=<?php echo $result[0] ?>" class="btn btn-warning text-white">แก้ไข</a></td> -->
-    <td align="right"> <a href="./deluser2.php?ID=<?php echo $result[0]; ?>" class="btn btn-danger">ลบ</a></td>
-  </tr>
-<?php
-$count=$count+1;
-}
-?>
-</table>
-<br>
-Total <?php echo $num_rows;?> Record : <?php echo $num_pages;?> Page :
-<?php
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">รายชื่อคณะครู บุคลากรผู้สมัครสมาชิก</h1>
+			<div class="card">
+				<div class="card-body">
+					<div class="table-responsive-xl">
+						<table align="center">
+							<thead>
+								<tr>
+									<th width="91"> <div align="center">ลำดับ</div></th>
+									<th width="110"> <div align="center">ชื่อผู้ใช้ </div></th>
+									<th width="120"> <div align="center">รหัสผ่าน </div></th>
+									<th width="150"> <div align="center">ชื่อ-นามสกุล </div></th>
+									<th width="80"><div align="center">สถานะผู้ใช้</div></th>
+									<th width="100"><div align="center">เริ่มสมัครสมาชิก</div></th>
+									<!-- <th width="59"> <div align="center">แก้ไข </div></th> -->
+									<th width="71"> <div align="center">ลบ </div></th>
+								</tr>
+							</thead>
+								<?php
+									$count = 1;
+									while($result=mysqli_fetch_array($query))
+								{
+								?>
+							<tbody>
+								<tr>
+									<td><div align="center"><?php echo $count;?></div></td>
+									<td><?php echo $result["Username"];?></td>
+									<td><?php echo $result["Password"];?></td>
+									<td><div align="center"><?php echo $result["Firstname"] ." ".$result["Lastname"];?></div></td>
+									<td><?php 
+										$level = $result["Userlevel"];
+										$sql1 = "SELECT * FROM `user_level` WHERE `userlevel_id` = $level ";
+										$query1 = mysqli_query($conn,$sql1);
+										$userlevel = mysqli_fetch_array($query1);
+										echo $userlevel[1];
+										
+										
+										?>
+									</td>
+									<td align="right"><?php echo $result["date"];?></td>
+									<!-- <td align="right"><a href="./edituser.php?ID=<?php echo $result[0] ?>" class="btn btn-warning text-white">แก้ไข</a></td> -->
+									<td align="right"> <a href="./deluser2.php?ID=<?php echo $result[0]; ?>" class="btn btn-danger">ลบ</a></td>
+								</tr>
+									<?php
+										$count=$count+1;
+										}
+									?>
+							</tbody>
+						</table>
+						<br>
+							Total <?php echo $num_rows;?> Record : <?php echo $num_pages;?> Page :
+							<?php
 
-if($prev_page)
-{
-	echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$prev_page'><< Back</a> ";
-}
+								if($prev_page)
+								{
+									echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$prev_page'><< Back</a> ";
+								}
 
-for($i=1; $i<=$num_pages; $i++){
-	if($i != $page)
-	{
-		echo "[ <a href='$_SERVER[SCRIPT_NAME]?Page=$i'>$i</a> ]";
-	}
-	else
-	{
-		echo "<b> $i </b>";
-	}
-}
-if($page!=$num_pages)
-{
-	echo " <a href ='$_SERVER[SCRIPT_NAME]?Page=$next_page'>Next>></a> ";
-}
-$conn = null;
-?>
- </body>
+								for($i=1; $i<=$num_pages; $i++){
+								if($i != $page)
+								{
+									echo "[ <a href='$_SERVER[SCRIPT_NAME]?Page=$i'>$i</a> ]";
+								}
+								else
+								{
+									echo "<b> $i </b>";
+								}
+								}
+								if($page!=$num_pages)
+								{
+									echo " <a href ='$_SERVER[SCRIPT_NAME]?Page=$next_page'>Next>></a> ";
+								}
+								$conn = null;
+							?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
