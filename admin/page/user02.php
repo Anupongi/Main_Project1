391efa8c213b96156bafa9e9d6c4ec01168e15e5
Icesolo <jmfairy123@hotmail.com>
2019-11-13 16:11:02 +0700

    lastupdate

--- admin/page/user02.php
+++ admin/page/user02.php
@@ -108,7 +108,32 @@
 								<?php
 								$count=$count+1;
 								}
-								?>		
+								?>	
+								<br>
+									Total <?php echo $num_rows;?> Record : <?php echo $num_pages;?> Page :
+									<?php
+
+									if($prev_page)
+									{
+										echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$prev_page'><< Back</a> ";
+									}
+
+									for($i=1; $i<=$num_pages; $i++){
+										if($i != $page)
+										{
+											echo "[ <a href='$_SERVER[SCRIPT_NAME]?Page=$i'>$i</a> ]";
+										}
+										else
+										{
+											echo "<b> $i </b>";
+										}
+									}
+									if($page!=$num_pages)
+									{
+										echo " <a href ='$_SERVER[SCRIPT_NAME]?Page=$next_page'>Next>></a> ";
+									}
+									$conn = null;
+									?>		
 							</tbody>
 						</table>
 					</div>
@@ -119,31 +144,7 @@
 </div>
 
 
-<br>
-Total <?php echo $num_rows;?> Record : <?php echo $num_pages;?> Page :
-<?php
 
-if($prev_page)
-{
-	echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$prev_page'><< Back</a> ";
-}
-
-for($i=1; $i<=$num_pages; $i++){
-	if($i != $page)
-	{
-		echo "[ <a href='$_SERVER[SCRIPT_NAME]?Page=$i'>$i</a> ]";
-	}
-	else
-	{
-		echo "<b> $i </b>";
-	}
-}
-if($page!=$num_pages)
-{
-	echo " <a href ='$_SERVER[SCRIPT_NAME]?Page=$next_page'>Next>></a> ";
-}
-$conn = null;
-?>
  </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
