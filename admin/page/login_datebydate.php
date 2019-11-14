105775a0ea076a6c9ed11a6f3a9b3241fa6349cd
Icesolo <jmfairy123@hotmail.com>
2019-11-14 09:37:44 +0700

    lastupdate

--- admin/page/login_datebydate.php
+++ admin/page/login_datebydate.php
@@ -52,7 +52,9 @@ $dbName = "user_login";
 $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 $date = date('d/m/Y');
 mysqli_set_charset($conn, "utf8");
-$sql = "SELECT * FROM `login_date`";
+if(isset($_POST['datetimepicker1'])){  
+  $date = $_POST['datetimepicker1'];
+  $sql = "SELECT * FROM `login_date` WHERE `Lastdate` ='$date'";
 $query = mysqli_query($conn,$sql);
 
 $num_rows = mysqli_num_rows($query);
@@ -90,6 +92,85 @@ $per_page = 10;   // Per Page
     
     $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
 	$query = mysqli_query($conn,$sql);
+
+}else{
+ $sql = "SELECT * FROM `login_date`";
+$query = mysqli_query($conn,$sql);
+
+$num_rows = mysqli_num_rows($query);
+
+$per_page = 10;   // Per Page
+	$page  = 1;
+	
+	if(isset($_GET["Page"]))
+	{
+		$page = $_GET["Page"];
+	}
+
+	$prev_page = $page-1;
+	$next_page = $page+1;
+
+	$row_start = (($per_page*$page)-$per_page);
+	if($num_rows<=$per_page)
+	{
+		$num_pages =1;
+	}
+	else if(($num_rows % $per_page)==0)
+	{
+		$num_pages =($num_rows/$per_page) ;
+	}
+	else
+	{
+		$num_pages =($num_rows/$per_page)+1;
+		$num_pages = (int)$num_pages;
+	}
+	$row_end = $per_page * $page;
+	if($row_end > $num_rows)
+	{
+		$row_end = $num_rows;
+	}
+    
+    $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
+	$query = mysqli_query($conn,$sql); 
+}
+// $sql = "SELECT * FROM `login_date`";
+// $query = mysqli_query($conn,$sql);
+
+// $num_rows = mysqli_num_rows($query);
+
+// $per_page = 10;   // Per Page
+// 	$page  = 1;
+	
+// 	if(isset($_GET["Page"]))
+// 	{
+// 		$page = $_GET["Page"];
+// 	}
+
+// 	$prev_page = $page-1;
+// 	$next_page = $page+1;
+
+// 	$row_start = (($per_page*$page)-$per_page);
+// 	if($num_rows<=$per_page)
+// 	{
+// 		$num_pages =1;
+// 	}
+// 	else if(($num_rows % $per_page)==0)
+// 	{
+// 		$num_pages =($num_rows/$per_page) ;
+// 	}
+// 	else
+// 	{
+// 		$num_pages =($num_rows/$per_page)+1;
+// 		$num_pages = (int)$num_pages;
+// 	}
+// 	$row_end = $per_page * $page;
+// 	if($row_end > $num_rows)
+// 	{
+// 		$row_end = $num_rows;
+// 	}
+    
+//     $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
+// 	$query = mysqli_query($conn,$sql);
 
 
 ?>
@@ -464,7 +545,7 @@ $per_page = 10;   // Per Page
                                         }else{
                                             echo "<script type='text/javascript'>";
 				                            echo "alert('ขออภัยค่ะ ! ไม่พบการลงชื่อเข้าให้ประจำวันนี้');";
-				                            echo "window.location='./login_datebyname.php';";
+				                            echo "window.location='./login_datebydate.php';";
 			                                echo "</script>";
                                         }
                                         }
