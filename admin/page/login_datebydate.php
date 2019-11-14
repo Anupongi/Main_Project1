c2cde8c979ee86dfdfbcb87759f8a6a0839badca
Icesolo <jmfairy123@hotmail.com>
2019-11-14 09:52:13 +0700

    lastupdate

--- admin/page/login_datebydate.php
+++ admin/page/login_datebydate.php
@@ -133,46 +133,6 @@ $per_page = 10;   // Per Page
     $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
 	$query = mysqli_query($conn,$sql); 
 }
-// $sql = "SELECT * FROM `login_date`";
-// $query = mysqli_query($conn,$sql);
-
-// $num_rows = mysqli_num_rows($query);
-
-// $per_page = 10;   // Per Page
-// 	$page  = 1;
-	
-// 	if(isset($_GET["Page"]))
-// 	{
-// 		$page = $_GET["Page"];
-// 	}
-
-// 	$prev_page = $page-1;
-// 	$next_page = $page+1;
-
-// 	$row_start = (($per_page*$page)-$per_page);
-// 	if($num_rows<=$per_page)
-// 	{
-// 		$num_pages =1;
-// 	}
-// 	else if(($num_rows % $per_page)==0)
-// 	{
-// 		$num_pages =($num_rows/$per_page) ;
-// 	}
-// 	else
-// 	{
-// 		$num_pages =($num_rows/$per_page)+1;
-// 		$num_pages = (int)$num_pages;
-// 	}
-// 	$row_end = $per_page * $page;
-// 	if($row_end > $num_rows)
-// 	{
-// 		$row_end = $num_rows;
-// 	}
-    
-//     $sql .= " ORDER BY ID ASC LIMIT $row_start ,$row_end ";
-// 	$query = mysqli_query($conn,$sql);
-
-
 ?>
 <div class="wrapper">
 
