e3252239fdd142eba19c4b15c7eeb88c14f79558
Icesolo <jmfairy123@hotmail.com>
2019-11-18 12:05:53 +0700

    lastupdate

--- admin/treatment/treatment_frm.php
+++ admin/treatment/treatment_frm.php
@@ -393,7 +393,7 @@ $num_rows2 = mysqli_num_rows($query2);
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
+<script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
