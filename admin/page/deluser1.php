fe3dc5ed8855da940b0107716aaf8d310e7a5f19
Icesolo <jmfairy123@hotmail.com>
2019-11-19 14:56:27 +0700

    lastupdate

--- admin/page/deluser1.php
+++ admin/page/deluser1.php
@@ -3,5 +3,5 @@
     $id =  $_GET['ID'];
     $sqlDele = "DELETE FROM `user` WHERE `ID` = $id";
     $queryDele = mysqli_query($con,$sqlDele);
-    header('Location: user02.php');
+    // header('Location: user02.php');
 ?>
