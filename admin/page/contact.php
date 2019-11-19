d767e8410dfe30bcafe21df5d0402dca0735a90c
Icesolo <jmfairy123@hotmail.com>
2019-11-19 23:39:32 +0700

    lastupdate

--- admin/page/contact.php
+++ admin/page/contact.php
@@ -173,131 +173,13 @@ session_start();
         </div>
         <!-- End Navbar -->
         <hr>
-        <!-- Carousel -->
-        <div class="row">
-            <div class="col-md-12">
-                <section>
-                    <div class="bd-example">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <?php
-                                    $sql = "SELECT * FROM `allpost` WHERE `published`= 'y' ORDER BY `date` DESC LIMIT 3";
-                                    $query = mysqli_query($conn, $sql);
-                                    $i = 0;
-                                    while ($d = mysqli_fetch_array($query)) {
-                                    $i = $i + 1;
-                                ?>
-                                <div class="carousel-item <?php if ($i == 1) { echo "active"; } ?>">
-                                    <img src="./admin/page/img/img/<?php echo $d[2] ?>" class="d-block w-100" style="filter: grayscale(10%);">
-        
-                                    <div class="carousel-caption d-none d-md-block" style="background: linear-gradient(to right, rgba(255,255,0), rgba(255,255,0,0.3));width: 90%;">
-                                        <a href="./previewcontentpost.php?id=<?php echo $d[0] ?> ">
-                                            <h3 style=""><?php echo $d[1]  ?></h3>
-                                            </h3>
-                                        </a>
-                                    </div>
-                                </div>
-                                <!-- <div class="carousel-item">
-                                    <img src="..." class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="..." class="d-block w-100" alt="...">
-                                </div> -->
-                                <?php
-                                }
-                                ?>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
         <!-- content -->
         <div class=" float-left mr-3 mt-3" style="height:60px;width:15px;background-color: #0064CA;">
         </div>
         <h1 class="p-3 display-4 content_text font">ข่าวประชาสัมพันธ์ </h1>
         <div class="row popular_text">
             <div class="col-sm-8 py-2">
-                <?php
                 
-
-                $results_per_page = 6;
-
-                $sql = "SELECT * FROM `allpost` WHERE `published` = 'y'";
-                $result = mysqli_query($conn, $sql);
-                $number_of_results = mysqli_num_rows($result);
-                $number_of_pages = ceil($number_of_results / $results_per_page);
-
-                if (!isset($_GET['page'])) {
-                    $page = 1;
-                } else {
-                    $page = $_GET['page'];
-                }
-
-                $this_page_first_result = ($page - 1) * $results_per_page;
-
-                $sql = "SELECT * FROM `allpost` WHERE `published` = 'y' ORDER BY `post_id` DESC LIMIT $this_page_first_result,$results_per_page";
-                $result = mysqli_query($conn, $sql);
-                while ($d = mysqli_fetch_array($result)) {
-                    ?>
-                    <div class="card float-left m-3 wow fadeInUp shadow p-3 " data-wow-duration="1s" style="width: 20rem;height: 21rem">
-                        <img class="card-img-top rounded" src="./admin/page/img/img/<?php echo $d[2] ?>" height="180" alt="Card image cap">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <!-- <a href="./pages/category.php?name=<?php echo $d[0] ?>" class="btn btn-primary btn-sm float-left" style="font-size:12px;"><?php echo $d[2] ?></a> -->
-                                    <h5 class="float-left m-1 text-muted" style="font-size:15px;"><?php echo $d[4] ?></h5>
-                                </div>
-
-                            </div>
-                            <div class="row pt-2">
-                                <div class="col-sm-12">
-                                    <h5><a href="./previewcontentpost.php?id=<?php echo $d[0] ?>"><?php echo $d[1] ?> </a></h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php
-                }
-                ?>
-                <?php
-                if ($number_of_results >= 7) {
-                    ?>
-                    <div class=" float-left  wow fadeInUp " data-wow-duration="1s" style="width: 95%;">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <nav aria-label="Page navigation example">
-                                        <ul class="pagination">
-                                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                            <?php
-                                            for ($page = 1; $page <= $number_of_pages; $page++) {
-                                                echo '<li class="page-item"><a class="page-link" href="index.php?page=' . $page . '">' . $page . '</a> </li>';
-                                            }
-                                            ?>
-                                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php
-                }
-                ?>
             </div>
 
             <div class="col-md-4">
@@ -318,10 +200,10 @@ session_start();
                             <div class="card-body">
                                 <div class="row ">
                                     <div class="col-lg-4" align="center">
-                                        <img src="./admin/page/img/img/<?php echo $d[2]; ?>" style="width:100px;height:80px;" >
+                                        <img src="./img/img/<?php echo $d[2]; ?>" style="width:100px;height:80px;" >
                                     </div>
                                     <div class="col-lg-8">
-                                        <h5> <a class="card-title" href="./previewcontentpost.php?id=<?php echo $d[0]; ?>"><?php echo $d[1]?></a></h5>
+                                        <h5> <a class="card-title" href="../../previewcontentpost.php?id=<?php echo $d[0]; ?>"><?php echo $d[1]?></a></h5>
                                         <p class="card-text"><small class="text-muted">วันที่ : <?php echo $d[4] ?></small></p>
                                     </div>
                                 </div>
@@ -358,13 +240,13 @@ session_start();
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-4" align="center">
-                                        <img src="./admin/page/upload_download/uploads/img/<?php echo $d[1] ?>" style="width:90px;height:90px;">
+                                        <img src="./upload_download/uploads/img/<?php echo $d[1] ?>" style="width:90px;height:90px;">
                                     </div>
                                     <div class="col-lg-8 text-center">
                                             <h6 class="card-title"><?php echo substr($d[2], 0, 88). "..." ?></h6>
                                             <h6 style="font-size:14px">จำนวนดาวน์โหลด : <?php echo $d[5]; ?> ครั้ง</h6>
                                             <!-- <a href="./admin/page/upload_download/uploads/file/<?php echo $d[2]?>" style="color:red;">Download</a> -->
-                                            <a href="./admin/page/upload_download/downloads.php?path=<?php echo $d[2]?>&id=<?php echo $d[0]?>" style="color:red;">Download</a>
+                                            <a href="./upload_download/downloads.php?path=<?php echo $d[2]?>&id=<?php echo $d[0]?>" style="color:red;">Download</a>
                                             <p class="card-text"><small class="text-muted">วันที่โพสต์ : <?php echo $d[7]; ?></small></p>
                                     </div>
                                 </div>
