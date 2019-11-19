db1f52a5005969e761cf65a994dea2acba659532
Icesolo <jmfairy123@hotmail.com>
2019-11-19 22:25:53 +0700

    lastupdate

--- admin/page/logindate_menu.php
+++ admin/page/logindate_menu.php
@@ -450,7 +450,7 @@ $per_page = 10;   // Per Page
                                         <td><div align="center"><?php echo $d[1] ." ".$d["Lastname"];?></div></td>
                                         <td><?php echo $d[3];?></td>
                                         <td align="right"><?php echo $d["time"];?></td>
-                                        <td align="right"> <a href="./del.php?ID=<?php echo $result['ID']; ?>" class="btn btn-danger">ลบ</a></td>
+                                        <td align="right"> <a href="./del.php?ID=<?php echo $d['ID']; ?>" class="btn btn-danger">ลบ</a></td>
                                     </tr>
                                 <?php
                                 }
