bc939d3abd442374228c82a7eb97b052d7725327
Icesolo <jmfairy123@hotmail.com>
2019-11-19 14:44:40 +0700

    lastupdate

--- admin/page/deluser1.php
+++ admin/page/deluser1.php
@@ -1,11 +1,7 @@
 <?php 
     include("../connection/connection.php");
     $id =  $_GET['ID'];
-        // $sql = "SELECT `img` FROM `tb_employee_6139010002` WHERE `id` = $id";
-        // $query = mysqli_query($con,$sql);
-        // $d= mysqli_fetch_array($query);
-        // unlink("./img/$d[0]");
     $sqlDele = "DELETE FROM `user` WHERE `ID` = $id";
     $queryDele = mysqli_query($con,$sqlDele);
-    header('Location: user02.php');
+    // header('Location: user02.php');
 ?>
