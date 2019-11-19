65cd4a657f1ae426ab3cdb5240827de2935edcee
Icesolo <jmfairy123@hotmail.com>
2019-11-19 21:45:37 +0700

    lastupdate

--- user/deleteuser.php
+++ user/deleteuser.php
@@ -2,16 +2,16 @@
     include "../connection/connection.php";
     $username = $_GET['username'];
     $sql="DELETE FROM user WHERE Username='$username'";
-    $query=mysqli_query($conn,$sql);
+    $query=mysqli_query($con,$sql);
     if($query==1){
         echo "<script>";
         echo "alert(\"ชื่อผู้ใช้นี้ถูกลบจากระบบแล้ว\");"; 
-        // echo "window.location.href='../Login/logout.php';";
+        echo "window.location.href='../Login/index.php';";
         echo "</script>";
     }else{
         echo "<script>";
         echo "alert(\"ชื่อผู้ใช้นี้ถูกลบจากระบบแล้ว\");"; 
-        // echo "window.history.back();";
+        echo "window.history.back();";
         echo "</script>";
     }
 ?>
