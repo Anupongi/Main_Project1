6bd697a67d69796918889237ba3d0ae863824a4c
Icesolo <jmfairy123@hotmail.com>
2019-11-20 13:20:46 +0700

    lastupdate

--- admin/treatment/delmed.php
+++ admin/treatment/delmed.php
@@ -4,10 +4,10 @@
     $userPassword = "Ice@2019";
     $dbName = "treatment";
     $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
-    $id =  $_POST['ID'];
+    $id =  $_GET['ID'];
     $sqlDele = "DELETE FROM `tb_med` WHERE `med_id` = $id";
     $queryDele = mysqli_query($con,$sqlDele);
     // echo $sqlDele;
-    header('Location: treatment_list.php');
+    // header('Location: treatment_list.php');
 
 ?>
