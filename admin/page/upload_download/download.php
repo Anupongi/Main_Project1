f0e1e59593913e879a879d4bba33687c23f77f0f
Icesolo <jmfairy123@hotmail.com>
2019-12-26 18:31:57 +0700

    lastupdate

--- admin/page/upload_download/download.php
+++ admin/page/upload_download/download.php
@@ -44,7 +44,7 @@
 <?php
 $serverName = "localhost";
 $userName = "root";
-$userPassword = "Ice@2019";
+$userPassword = "itcmtc2019";
 $dbName = "file-management";
 $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
