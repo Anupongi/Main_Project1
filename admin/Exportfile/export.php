99cc1ca778879e1981bbac8e17e102274e9f9faf
Icesolo <jmfairy123@hotmail.com>
2019-12-26 18:11:27 +0700

    lastupdate

--- admin/Exportfile/export.php
+++ admin/Exportfile/export.php
@@ -16,9 +16,9 @@
     $excelObj = $excelReader->load("../Exportfile/doc_xlsxflie/รายชื่อผู้ลงชื่อเข้าใช้ประจำวัน.xlsx");
     $excelObj->setActiveSheetIndex(0);
     mysqli_select_db($conn, 'user_login');
-    echo $date;
+    
     $sql_vs = "SELECT login_date.Username,user.Firstname, user.Lastname,login_date.Lastdate,login_date.time FROM user INNER JOIN login_date ON user.Username=login_date.Username WHERE login_date.`Lastdate` = '$date' ORDER BY `login_date`.`time` DESC";
-    // echo $sql_vs;
+    echo $sql_vs;
     $query_vs = mysqli_query($conn, $sql_vs);
     
     
