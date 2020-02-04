7d9b7e60a3b283593f4df2b5b310d10d87ed3f6d
Icesolo <jmfairy123@hotmail.com>
2020-02-04 22:34:50 +0700

    last

--- admin/page/editpage/hidden.php
+++ admin/page/editpage/hidden.php
@@ -9,5 +9,6 @@ $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 $id=$_GET['id'];
 $sql = "UPDATE `allpost` SET `published`='n' WHERE `post_id` = $id";
 $query = mysqli_query($conn,$sql);
-Header("Location: ../allpost.php"); 
+echo $sql;
+// Header("Location: ../allpost.php"); 
 ?>
